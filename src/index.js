ce5cf085a120c5a7ecab36826aacbe7244cbc511
YanJin <yanjin.cheng@scality.com>
2020-02-19 14:10:53 +0100

    add navbar to the shell
    - install HtmlWebpackPlugin
    - add navbar from core-ui
    - add multiple loader inside the module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Navbar } from '@scality/core-ui';
 
-ReactDOM.render(<div>hello world!!</div>, document.getElementById('root'));
+const tabs = [
+  {
+    selected: false,
+    title: 'metalK8s',
+    onClick: () => console.log('metalK8s'),
+  },
+  {
+    selected: false,
+    title: 'Zenko',
+    onClick: () => console.log('zenko'),
+  },
+];
+
+ReactDOM.render(
+  <Navbar productName={'ShellPOC'} tabs={tabs} />,
+  document.getElementById('root'),
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -18,6 +19,24 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
     ],
   },
+  devServer: {
+    compress: true,
+    port: 3000,
+  },
+  // `HtmlWebpackPlugin` will generate index.html into the build folder
+  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
 };
